6149948cf7b87efd5fa950eda845204d0c9cc416
saifmohammad00 <saifmohammad592001@gmail.com>
2024-07-22 14:45:05 +0530

    Managing the token

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,11 +1,13 @@
-import { useState, useRef } from 'react';
+import { useState, useRef,useContext } from 'react';
 
 import classes from './AuthForm.module.css';
+import AuthContext from '../../store/auth-context';
 
 const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
+  const authCtx= useContext(AuthContext);
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -46,7 +48,7 @@ const AuthForm = () => {
       }
     })
     .then((data)=>{
-      console.log(data);
+      authCtx.login(data.idToken);
     })
     .catch(error=>{
       alert(error.message);
--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -1,8 +1,17 @@
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
 
 import classes from './MainNavigation.module.css';
+import AuthContext from '../../store/auth-context';
 
 const MainNavigation = () => {
+  
+  const authCtx = useContext(AuthContext);
+  const isLoggedIn = authCtx.isLoggedIn;
+  const handleLogout=(event)=>{
+    event.preventDefault();
+    authCtx.logout();
+  }
   return (
     <header className={classes.header}>
       <Link to='/'>
@@ -10,15 +19,16 @@ const MainNavigation = () => {
       </Link>
       <nav>
         <ul>
-          <li>
+          {!isLoggedIn && <li>
             <Link to='/auth'>Login</Link>
-          </li>
-          <li>
+          </li>}
+          {isLoggedIn && <li>
             <Link to='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          </li>}
+          {isLoggedIn &&
+            <li>
+              <button onClick={handleLogout}>Logout</button>
+            </li>}
         </ul>
       </nav>
     </header>
