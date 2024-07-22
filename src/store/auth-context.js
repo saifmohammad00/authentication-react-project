93a3f07b899ccbdb4ec12936ad093aa1b5b203fb
saifmohammad00 <saifmohammad592001@gmail.com>
2024-07-22 16:54:31 +0530

    Auto logout

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,27 +1,42 @@
-import React, { useState } from "react";
-const AuthContext=React.createContext({
-     token:"",
-     isLoggedIn:false,
-     login:(token)=>{},
-     logout:()=>{}
+import React, { useEffect, useState } from "react";
+const AuthContext = React.createContext({
+    token: "",
+    isLoggedIn: false,
+    login: (token) => { },
+    logout: () => { }
 });
-export const AuthContextProvider=(props)=>{
-    const initialToken=localStorage.getItem("token");
-    const [token,setToken]=useState(initialToken);
-    const userIsLoggenIn=!!token;
-    const loginHandler=(token)=>{
+export const AuthContextProvider = (props) => {
+    const initialToken = localStorage.getItem("token");
+    const [token, setToken] = useState(initialToken);
+    const userIsLoggenIn = !!token;
+
+    useEffect(() => {
+        if (token) {
+            const clearTokenAfterTimeout = () => {
+                localStorage.removeItem("token");
+
+                alert("Your session has expired. Please log in again.");
+            };
+            const timeout = setTimeout(clearTokenAfterTimeout, 5*60 * 1000);
+            return () => {
+                clearTimeout(timeout);
+            };
+        }
+    }, [token]);
+
+    const loginHandler = (token) => {
         setToken(token);
-        localStorage.setItem("token",token);
+        localStorage.setItem("token", token);
     }
-    const logoutHandler=()=>{
+    const logoutHandler = () => {
         setToken(null);
         localStorage.removeItem("token");
     }
-    const contextValue={
-        token:token,
-        isLoggedIn:userIsLoggenIn,
-        login:loginHandler,
-        logout:logoutHandler
+    const contextValue = {
+        token: token,
+        isLoggedIn: userIsLoggenIn,
+        login: loginHandler,
+        logout: logoutHandler
     }
     return <AuthContext.Provider value={contextValue}>
         {props.children}
